7ffcc82d0344d1131a50dc517d200b7b5bee205c
mariuscolac261 <marius-daniel.colac@s.unibuc.ro>
2022-12-22 18:45:08 +0200

    Tema2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, world!\n";
-    return 0;
+#include "Programator.h"
+int main()
+{
+ Programator p1{"3", {Contabil{"Adrian", 20, 80}.clone()}
+};
+ std::cout <<  p1.salariuF();
 }
